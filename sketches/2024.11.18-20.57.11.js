5ed70a2d7cdec43632044e9808da9aaf9da8a31f
gwebsurfer <gab@gab.art.br>
2024-11-18 22:16:36 -0300

    feat: change sketch colors

--- sketches/2024.11.18-20.57.11.js
+++ sketches/2024.11.18-20.57.11.js
@@ -1,24 +1,26 @@
-const canvasSketch = require('canvas-sketch');
+import canvasSketch from 'canvas-sketch';
 
 const settings = {
   dimensions: [1080, 1080],
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
+const sketch = ({ context, width, height }) => {
+  const w = width * 0.1;
+  const h = height * 0.1;
+  const gap = width * 0.033;
+  const initialX = width * 0.18;
+  const initialY = height * 0.18;
+
+  const off = width * 0.02;
+
+  let x, y;
+
+  return () => {
     context.fillRect(0, 0, width, height);
     context.lineWidth = width * 0.01;
 
-    const w = width * 0.1;
-    const h = height * 0.1;
-    const gap = width * 0.033;
-    const initialX = width * 0.18;
-    const initialY = height * 0.18;
-
-    const off = width * 0.02;
-
-    let x, y;
+    context.fillStyle = 'black';
+    context.strokeStyle = 'white';
 
     for (let i = 0; i < 5; i++) {
       for (let j = 0; j < 5; j++) {
